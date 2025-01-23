00497db443528d6b6d3c34c85b21a63f4b6b5dd7
hihihi-hihi <elliotshin0101@gmail.com>
2025-01-23 10:10:44 +0900

    1st commit
    First commit

---
+++ tips_load.py
@@ -0,0 +1,13 @@
+import seaborn as sns
+
+def get_tips():
+    tips = sns.load_dataset('tips')
+    return tips
+
+
+
+def print_get():
+    return '일어나세여 용사여'
+
+if __name__ == '__main__':
+    print_get()
